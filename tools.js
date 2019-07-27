faa39c265d7b0172049065d5c728aa21c7aa3457
Dylan Sbogar <s3718036@student.rmit.edu.au>
2019-07-28 08:29:24 +1000

    css fixes

--- tools.js
+++ tools.js
@@ -1,9 +1,11 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "350px";
+  document.getElementById("headerLogo").style.marginLeft = "300px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("headerLogo").style.marginLeft = "0";
 }
 
 function checkIfLogged() {
